3d449808433c92ccbdad6b34b83a2f30172cc9e3
Árni Gunnarsson <arnigunn@hi.is>
2026-04-19 12:03:45 +0000

    Fix cache keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,23 +22,7 @@ class User < ApplicationRecord
   #
 
   def upcoming_matches
-    Match.upcoming_matches(competitions)
+    Match.upcoming_matches(competitions.ids)
          .includes(:home_team, :away_team, :competition)
-  end
-
-  def incomplete_matches
-    # Use the user's latest prediction update to automatically expire the cache
-    last_change = predictions.maximum(:updated_at).to_i
-    cache_key = "user/#{id}/incomplete/#{match_ids.hash}/#{last_change}"
-
-    Rails.cache.fetch(cache_key, expires_in: 1.hour) do
-      incomplete_ids = Match.find_by_sql([
-                                           'SELECT id FROM user_incomplete_matches(?, ARRAY[?]::BIGINT[])',
-                                           id,
-                                           upcoming_matches.ids
-                                         ]).map(&:id)
-
-      Match.where(id: incomplete_ids)
-    end
   end
 end
