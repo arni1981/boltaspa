f071051115a13e4f9a47391c5119167a56142468
Árni Gunnarsson <arnigunn@hi.is>
2026-04-21 15:29:04 +0000

    Locale route scopes

--- config/initializers/timezone_and_locales.rb
+++ config/initializers/timezone_and_locales.rb
@@ -1,3 +1,3 @@
 Rails.application.config.i18n.default_locale = :is
-
+Rails.application.config.i18n.available_locales = %i[is en]
 Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
