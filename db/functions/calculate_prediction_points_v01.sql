340906358cee3c2771150d1b863b7f3f869e4809
Árni Gunnarsson <arnigunn@hi.is>
2026-04-21 11:46:22 +0000

    Scoring function

--- db/functions/calculate_prediction_points_v01.sql
+++ db/functions/calculate_prediction_points_v01.sql
@@ -6,21 +6,26 @@ CREATE OR REPLACE FUNCTION public.calculate_prediction_points(home_score integer
 AS
 $function$
 SELECT CASE
-           WHEN home_score = home_guess AND away_score = away_guess THEN 25
+  WHEN home_score = home_guess AND away_score = away_guess THEN 25
 
-           ELSE
-               (
-                   CASE
-                       WHEN SIGN(home_score - away_score) = SIGN(home_guess - away_guess)
-                           THEN 10
-                       ELSE 0
-                       END
-                       +
-                   CASE
-                       WHEN SIGN(home_score - away_score) = SIGN(home_guess - away_guess)
-                           THEN GREATEST(0, 10 - (ABS(home_score - home_guess) + ABS(away_score - away_guess)) * 2)
-                       ELSE GREATEST(0, 6 - (ABS(home_score - home_guess) + ABS(away_score - away_guess)) * 2)
-                       END
-                   )
-           END;
+  ELSE
+    (
+      -- Outcome
+      CASE
+        WHEN SIGN(home_score - away_score) = SIGN(home_guess - away_guess)
+          THEN 10
+        ELSE 0
+      END
+
+      +
+
+      -- Closeness (with light GD penalty)
+      GREATEST(
+        0,
+        10
+        - (ABS(home_score - home_guess) + ABS(away_score - away_guess)) * 2
+        - ABS((home_score - away_score) - (home_guess - away_guess)) * 1
+      )
+    )
+END;
 $function$
