c8ef28b55c34d67648cf2e85215d458414d63431
Árni Gunnarsson <arnigunn@hi.is>
2026-04-28 19:45:57 +0000

    Gems

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ end
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'rack-mini-profiler'
   gem 'web-console'
 end
 
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,12 @@ class DashboardController < ApplicationController
 
     @past_matches = Current.user.matches.with_results.ordered.limit(15)
     @upcoming_matches = Current.user.matches.upcoming.ordered.limit(15)
-    @leagues = Current.user.leagues
     @next_match = @upcoming_matches.first
+
+    @leagues = Current.user
+                      .leagues
+                      .includes(league_competitions: %i[competition season standings])
+                      .includes(:members)
 
     @predictions_map = Current.user.predictions
                               .where(match_id: @past_matches.map(&:id))
