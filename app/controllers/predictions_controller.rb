eb4351d98f90c493741dea71069c2413fcaa4aa3
Árni Gunnarsson <arnigunn@hi.is>
2026-05-01 19:56:08 +0000

    Live saving

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -2,43 +2,22 @@ class PredictionsController < ApplicationController
   def index
     redirect_to onboarding_path and return if Current.user.leagues.none?
 
-    init_view_data
+    @upcoming_matches = Current.user.upcoming_matches
+    @predictions_map = Current.user.predictions_map(@upcoming_matches)
+    @unfinished_matches = @upcoming_matches.reject { |m| @predictions_map[m.id]&.home_guess.present? }
   end
 
   def create
-    @updated_match_ids = []
+    score = params[:score_string]
+    prediction = Current.user.predictions.find_or_initialize_by(match_id: params[:match_id])
 
-    ActiveRecord::Base.transaction do
-      params[:predictions].each do |match_id, data|
-        score = data[:score_string]
-        prediction = Current.user.predictions.find_or_initialize_by(match_id: match_id)
+    home, away = score.split('-')
+    prediction.update!(home_guess: home, away_guess: away)
 
-        if score.blank?
-          prediction&.destroy
-        else
-          home, away = score.split('-')
-          prediction.assign_attributes(home_guess: home, away_guess: away)
+    @match = Match.find(params[:match_id])
 
-          if prediction.changed?
-            prediction.save!
-            @updated_match_ids << match_id
-          end
-        end
-      end
-    end
-
-    @updated_matches = Match.where(id: @updated_match_ids)
-    init_view_data
-  end
-
-  def predictions_map
-    @predictions_map ||= Current.user.predictions
-                                .where(match_id: Current.user.upcoming_matches.map(&:id))
-                                .index_by(&:match_id)
-  end
-
-  def init_view_data
     @upcoming_matches = Current.user.upcoming_matches
-    @predictions_map = predictions_map
+    @predictions_map = Current.user.predictions_map(@upcoming_matches)
+    @unfinished_matches = @upcoming_matches.reject { |m| @predictions_map[m.id]&.home_guess.present? }
   end
 end
