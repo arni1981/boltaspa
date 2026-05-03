71763daab7cf7a570acefc93bc35c8b11ab71c0e
Árni Gunnarsson <arnigunn@hi.is>
2026-05-03 14:18:36 +0000

    Work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,17 +2,12 @@ class DashboardController < ApplicationController
   def show
     redirect_to onboarding_path and return if Current.user.leagues.none?
 
-    @past_matches = Current.user.matches.with_results.ordered.limit(15)
-    @upcoming_matches = Current.user.matches.upcoming.ordered.limit(15)
-    @next_match = @upcoming_matches.first
+    upcoming_matches = Current.user.matches.upcoming.ordered.limit(15)
+    @next_match = upcoming_matches.first
 
     @leagues = Current.user
                       .leagues
                       .includes(league_competitions: %i[competition season standings])
                       .includes(:members)
-
-    @predictions_map = Current.user.predictions
-                              .where(match_id: @past_matches.map(&:id))
-                              .index_by(&:match_id)
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,14 +3,14 @@ enable_integrity!
 
 # Core application and framework libraries
 pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin '@hotwired/stimulus', to: 'stimulus.min.js'
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: false
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: false
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: false
 
 # JavaScript controllers and custom code
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin_all_from 'app/javascript/custom', under: 'custom'
+pin_all_from 'app/javascript/controllers', under: 'controllers', preload: false
+pin_all_from 'app/javascript/custom', under: 'custom', preload: false
 pin_all_from 'app/javascript/debugging', under: 'debugging', preload: false
 
 # Third-party libraries
-pin 'sweetalert2' # @11.26.24
+pin 'sweetalert2', preload: false # @11.26.24
