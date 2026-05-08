f78c774eb86bea7bd8bf51173d008be66b865b24
Árni Gunnarsson <arnigunn@hi.is>
2026-05-08 14:19:31 +0000

    Google signin

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET']
+  provider :google_oauth2, Rails.app.creds('google_id'), Rails.app.creds('google_secret')
 end
