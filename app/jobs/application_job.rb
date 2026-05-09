ed3df0f448031aebcb476f7e1623fd170cc98570
Árni Gunnarsson <arnigunn@hi.is>
2026-05-09 16:08:38 +0000

    Work

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,15 +1,19 @@
 class ApplicationJob < ActiveJob::Base
-  BASE_URL = "https://api.football-data.org/v4/competitions/".freeze
+  BASE_URL = 'https://api.football-data.org/v4/competitions/'.freeze
 
   def football_data(endpoint)
     url = BASE_URL + endpoint
 
     connection = Faraday.new(url: url) do |f|
-      f.headers["X-Auth-Token"] = Rails.app.creds.require('FOOTBALL_DATA_AUTH_TOKEN'.downcase)
+      f.headers['X-Auth-Token'] = Rails.app.creds.require('FOOTBALL_DATA_AUTH_TOKEN'.downcase)
       f.request :json
       f.response :json
     end
 
-    connection.get.body
+    response = connection.get.body
+
+    Rails.logger.info "[API]: #{response.inspect}"
+
+    response
   end
 end
