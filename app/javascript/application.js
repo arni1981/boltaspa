129a1b9d9c0943d9ab5eedd86ea9001d61bde67a
Árni Gunnarsson <arnigunn@hi.is>
2026-05-23 20:15:52 +0000

    Refactor predictions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,26 +3,3 @@ import "controllers"
 
 import 'debugging/hotwire'
 import 'custom/turbo_stream_actions'
-
-/*
-    This might feel hacky, I would like to make this more pragmatic.
-    This hides the save predictions button if turbo is available since live saving 
-    should be the default 
-*/
-if (!isSavingData() && !hasSlowInternet()) {
-    ['turbo:render', 'turbo:load'].forEach(eventType => {
-        addEventListener(eventType, (event) => {
-            document.getElementById('sticky_bottom')?.classList.add('hidden')
-        })
-    });
-}
-
-function isSavingData() {
-    return navigator.connection?.saveData
-}
-
-function hasSlowInternet() {
-    return navigator.connection?.effectiveType === "slow-2g" ||
-        navigator.connection?.effectiveType === "2g" ||
-        navigator.connection?.effectiveType === "3g"
-}
--- app/javascript/controllers/auto_submit_controller.js
+++ app/javascript/controllers/auto_submit_controller.js
@@ -3,20 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="auto-submit"
 export default class extends Controller {
   submit(event) {
-    if (this.#isSavingData || this.#hasSlowInternet) {
-      return
-    }
-
     this.element.requestSubmit()
-  }
-
-  #isSavingData() {
-    return navigator.connection?.saveData
-  }
-
-  #hasSlowInternet() {
-    return navigator.connection?.effectiveType === "slow-2g" ||
-      navigator.connection?.effectiveType === "2g" ||
-      navigator.connection?.effectiveType === "3g"
   }
 }
