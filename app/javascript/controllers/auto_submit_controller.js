684943c1561a25d1b4935946e50c8e687c1a8f58
Árni Gunnarsson <arnigunn@hi.is>
2026-05-23 20:10:15 +0000

    Refactor predictions

--- app/javascript/controllers/auto_submit_controller.js
+++ app/javascript/controllers/auto_submit_controller.js
@@ -2,7 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="auto-submit"
 export default class extends Controller {
-  submit() {
+  submit(event) {
+    if (this.#isSavingData || this.#hasSlowInternet) {
+      return
+    }
+
     this.element.requestSubmit()
+  }
+
+  #isSavingData() {
+    return navigator.connection?.saveData
+  }
+
+  #hasSlowInternet() {
+    return navigator.connection?.effectiveType === "slow-2g" ||
+      navigator.connection?.effectiveType === "2g" ||
+      navigator.connection?.effectiveType === "3g"
   }
 }
