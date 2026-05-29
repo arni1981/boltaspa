825637a41c62c8dca9a64eabd45016817d563028
Árni Gunnarsson <arnigunn@hi.is>
2026-05-29 20:43:25 +0000

    Predictions page

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,9 +1,9 @@
 module TeamsHelper
   def localized_tla(team)
-    I18n.t("team_names.#{team.tla.downcase}.tla")
+    I18n.t("team_names.#{team.tla.downcase}.tla", default: team.tla)
   end
 
   def localized_short_name(team)
-    I18n.t("team_names.#{team.tla.downcase}.name")
+    I18n.t("team_names.#{team.tla.downcase}.name", default: team.short_name)
   end
 end
