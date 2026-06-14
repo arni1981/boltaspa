1df5c29dd4c1e8f369679ccc7599ff3b1f34cd49
Árni Gunnarsson <arnigunn@hi.is>
2026-06-14 10:21:09 +0000

    Localized competition names

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -15,4 +15,13 @@ class Competition < ApplicationRecord
   def prev_matchday(from_date = Time.current)
     self.matches.where(kickoff_at: ...from_date).limit(1).pluck('max(kickoff_at::date)').first
   end
+
+  def localized_name(with_year: true)
+    if with_year
+      I18n.t(code.downcase, scope: 'competitions.names', year: current_season.year,
+                            year_end: current_season.year + 1)
+    else
+      I18n.t(code.downcase, scope: 'competitions.names', year: nil, year_end: nil)
+    end
+  end
 end
