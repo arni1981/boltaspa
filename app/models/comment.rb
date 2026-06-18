84623b33a86e58e299c28af05e011b87b2eb15ae
Árni Gunnarsson <arnigunn@hi.is>
2026-06-18 20:47:53 +0000

    Comment broadcast

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -17,9 +17,11 @@ class Comment < ApplicationRecord
     )
 
     league_competition.members.each do |member|
+      # next if member.id == Current.user.id
+
       broadcast_replace_later_to(
-        "unread_messages_#{member.id}_#{self.league_competition.id}",
-        target: ActionView::RecordIdentifier.dom_target(member, self.league_competition, :unread_dot),
+        "unread_messages_#{self.league_competition.signed_id}",
+        target: ActionView::RecordIdentifier.dom_target(self.league_competition, :unread_dot),
         partial: 'league_competitions/unread_dot',
         locals: { league_competition: self.league_competition, unread_messages: true, user: member }
       )
